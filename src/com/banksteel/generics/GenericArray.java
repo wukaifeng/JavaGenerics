f90fddb1d5e751ca47161b4152277f2287dc3aba
wukaifeng <1076208637@qq.com>
2019-10-30 19:01:28 +0800

    添加测试代码

--- src/com/banksteel/generics/GenericArray.java
+++ src/com/banksteel/generics/GenericArray.java
@@ -1,5 +1,11 @@
 package com.banksteel.generics;
 
+/**
+ * 测试泛型数组
+ * @author wukaifeng
+ *
+ * @param <T>
+ */
 public class GenericArray<T> {
 
     private Object[] array;
@@ -20,12 +26,27 @@ public class GenericArray<T> {
         return (T[]) array;
     }
     
+    public Object[] rep2() {
+        return array;
+    }
+    
     public static void main(String[] args) {
         GenericArray<Integer> ga = new GenericArray<>(1);
         ga.put(0, 12);
         Integer integer = ga.get(0);
         System.out.println("integer:" + integer);
-        Integer[] rep = ga.rep();
-        System.out.println("rep:" + rep.toString());
+       // 下面不可强转
+        // Integer[] rep2 = ga.rep();
+        Object[] rep = ga.rep();
+        for (int i = 0; i < rep.length; i++) {
+            Integer in = (Integer) rep[i];
+            System.out.println("in:" + in);
+        }
+        
+        Object[] objArray = new Object[5];
+        objArray[0] = 1;
+        // 同样是不可强转
+        // Integer[] integerArray = (Integer[]) objArray;
+       
     }
 }
